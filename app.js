a888cab62a440082d7977df13b2e6ac80fefe38d
mira-kine <mirak0419@gmail.com>
2021-10-08 13:21:24 -0700

    added DOM elements, event listener, button and random generator are working. TDD next

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import functions and grab DOM elements
-
+const genButton = document.getElementById('generate');
+const randomNumText = document.getElementById('random-num');
 // initialize global state
 
 // set event listeners 
@@ -7,3 +8,7 @@
 // use user input to update state 
 // update DOM to reflect the new state
 
+genButton.addEventListener ('click', ()=> {
+    const randomNum = Math.floor(Math.random() * 100);
+    randomNumText.textContent = randomNum;
+});
